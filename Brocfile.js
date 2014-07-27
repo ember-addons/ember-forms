55201fe1e3ca8ebe2d24b4a627f81dd1eaeb4adb
Emerson Lackey <emerson.lackey@gmail.com>
2014-07-27 11:39:21 -0400

    Bulk of the migration is complete
    Still need to get the tests working.

--- Brocfile.js
+++ Brocfile.js
@@ -1,15 +1,13 @@
-//-- Require our broccoli toolchain ---------------------------------------
+//-- Require our broccoli build tools -------------------------------------
 var filterTemplates = require('broccoli-template');
 var mergeTrees = require('broccoli-merge-trees');
 var makeModule = require('broccoli-dist-es6-module');
 var pickFiles = require('broccoli-static-compiler');
 var concatTree = require('broccoli-concat');
-var templateCompiler = require('broccoli-ember-hbs-template-compiler');
 
 
 //-- Build Ember-Forms as amd, cjs, and global modules --------------------
 var app = 'lib';
-var vendor = 'vendor';
 
 function preprocess (tree) {
   tree = filterTemplates(tree, {
@@ -32,9 +30,10 @@ var appModule = makeModule(app, {
 
 
 //-- Vendor dependencies used by our tests and docs -----------------------
+var vendor = 'vendor';
 var vendorFiles = [
   'jquery/dist/jquery.js',
-  'handlebars/handlebars.runtime.js',
+  'handlebars/handlebars.js',
   'ember/ember.js',
   'ember-data/ember-data.js',
   'highlightjs/highlight.pack.js',
