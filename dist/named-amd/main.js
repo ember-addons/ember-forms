7f7248b300459e64ecf08073da6b6d52a83340c5
Felix Fichte <dev@spruce.de>
2014-12-02 22:24:36 +0100

    actually building dist

--- dist/named-amd/main.js
+++ dist/named-amd/main.js
@@ -766,7 +766,7 @@ define("ember-forms/templates/components/formgroup/form-group-control",
   ["exports"],
   function(__exports__) {
     "use strict";
-    __exports__["default"] = Ember.Handlebars.compile("{{#if controlWrapper}}\n    <div {{bind-attr class=controlWrapper}}>\n        {{view controlView viewName=controlViewName property=propertyName id=cid}}\n    </div>\n{{else}}\n    {{view controlView viewName=controlViewName property=propertyName id=cid}}\n{{/if}}");
+    __exports__["default"] = Ember.Handlebars.compile("{{#if controlWrapper}}\n    <div {{bind-attr class=controlWrapper}}>\n        {{#if data-html}}\n            {{view controlView viewName=controlViewName property=propertyName id=cid data-html=data-html data-placement=data-placement data-toggle=data-toggle title=title}}\n        {{else}}\n            {{view controlView viewName=controlViewName property=propertyName id=cid}}\n        {{/if}}\n    </div>\n{{else}}\n    {{#if data-html}}\n        {{view controlView viewName=controlViewName property=propertyName id=cid data-html=data-html data-placement=data-placement data-toggle=data-toggle title=title}}\n    {{else}}\n        {{view controlView viewName=controlViewName property=propertyName id=cid}}\n    {{/if}}\n{{/if}}\n\n");
   });
 define("ember-forms/templates/components/formgroup/form-group",
   ["exports"],
